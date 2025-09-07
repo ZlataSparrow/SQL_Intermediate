0ebc585286c6098563d0de7c642c528048037a4f
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-07 03:14:17 +0000

    stat functions

--- Exercise Files/Main_queries.sql
+++ Exercise Files/Main_queries.sql
@@ -5,6 +5,16 @@ FROM data_sci.employees
 SELECT count(*), min(id) , max(id)
 FROM data_sci.employees
 
-SELECT count(*)
+SELECT *
 FROM data_sci.employees
+LIMIT 1000
 
+SELECT department_id, SUM(salary), ROUND(AVG(SALARY),2)
+FROM data_sci.employees
+GROUP BY department_id
+
+SELECT department_id, SUM(salary), 
+ROUND(AVG(SALARY),2) AVG_SALARY, 
+ROUND(VAR_POP(SALARY),2) VAR_SALARY
+FROM data_sci.employees
+GROUP BY department_id
