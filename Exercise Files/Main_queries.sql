3de32f6ae31352ab65d4f1a400a723ae164e2953
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-07 04:02:13 +0000

    query1

--- Exercise Files/Main_queries.sql
+++ Exercise Files/Main_queries.sql
@@ -19,3 +19,11 @@ ROUND(VAR_POP(SALARY),2) VAR_SALARY,
 round(stddev_pop(salary),2) stddev_salary
 FROM data_sci.employees
 GROUP BY department_id
+
+SELECT last_name, department_id
+FROM data_sci.employees
+where last_name = 'boyd'
+
+SELECT last_name, department_id
+FROM data_sci.employees
+where last_name like 'b%'
