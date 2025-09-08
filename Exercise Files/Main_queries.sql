ba426eb68afa845707641b5c19798071ccf30555
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-08 05:09:52 +0000

    new table

--- Exercise Files/Main_queries.sql
+++ Exercise Files/Main_queries.sql
@@ -82,29 +82,100 @@ FROM data_sci.employees as e
 WHERE e.salary > 120000
 
 create table check1 (
-	id integer,
-    salary integer,
-    primary key (id)
+  id integer,
+  name CHAR(50),
+  salary integer,
+  departmentId INTEGER,
+  primary key (id)
   );
 
-insert into check1 values (1, 100);
-insert into check1 values (2, 100);
-insert into check1 values (3, 50);
+ create table check2 (
+  id integer,
+  name CHAR(50),
+  primary key (id)
+  ); 
 
+DROP TABLE check1;
+
+insert into check1 values (1, 'Joe', 60000, 1);
+insert into check1 values (2, 'Sam', 50000, 1);
+insert into check1 values (4, 'Max', 50000, 2);
+
+insert into check2 values (1, 'IT');
+insert into check2 values (2, 'HR');
+
+DELETE FROM check1
+WHERE id in (1,2,3);
 
 select * from check1;
+select * from check2;
+
+
+
+
 
 SELECT 
-  MAX(CASE WHEN salary_rank = 2 THEN salary ELSE NULL END) AS SecondHighestSalary,
+  MAX(CASE WHEN salary_rank = 2 THEN salary ELSE NULL END) AS SecondHighestSalary
 FROM (
   SELECT 
     salary,
-    RANK() OVER (ORDER BY salary DESC) AS salary_rank
+    RANK() OVER (ORDER BY  salary DESC, id DESC) AS salary_rank
   FROM check1
 ) ranked;
 
 
 SELECT 
-    salary,
-    RANK() OVER (ORDER BY salary DESC) AS salary_rank
+salary, 
+COUNT(*) AS count,
+RANK() OVER (ORDER BY  salary DESC) AS salary_rank
+FROM check1
+GROUP BY salary
+HAVING COUNT(*) > 1;
+
+
+
+SELECT 
+  MAX(
+    CASE 
+      WHEN salary_rank = 1 AND count = 1 THEN Null
+      WHEN salary_rank = 1 AND count = 2 THEN next_salary
+      WHEN salary_rank = 2 AND count = 2 THEN salary
+      WHEN salary_rank = 2 AND count = 1 THEN salary
+      ELSE NULL
+    END
+  ) AS SecondHighestSalary
+FROM (
+  SELECT 
+    salary, 
+    COUNT(*) AS count,
+    RANK() OVER (ORDER BY salary DESC) AS salary_rank,
+    LEAD(salary) OVER (ORDER BY salary DESC) AS next_salary
   FROM check1
+  GROUP BY salary
+) sub;
+
+
+SELECT 
+    salary,
+    RANK() OVER (ORDER BY salary DESC) AS salary_rank,
+    COUNT(*) AS count,
+    LEAD(salary) OVER (ORDER BY salary DESC) AS next_salary
+  FROM check1
+  GROUP BY salary
+
+
+
+
+SELECT 
+  salary,
+  ROW_NUMBER() OVER (ORDER BY salary DESC) AS salary_rank
+FROM check1;
+
+SELECT *
+FROM check1
+WHERE id IN (
+  SELECT id
+  FROM check1
+  GROUP BY id
+  HAVING COUNT(*) > 1
+);
