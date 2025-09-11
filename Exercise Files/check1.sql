4f1704b14caa4200bded67ba3161c0885b273ba9
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-11 21:58:47 +0000

    check

--- Exercise Files/check1.sql
+++ Exercise Files/check1.sql
@@ -2,8 +2,16 @@
 select * from check1
 
 
+select count(d_rank), d_rank 
+from 
+(SELECT id, num,
+Dense_RANK() OVER (ORDER BY id) AS d_rank
+  FROM check1
+  order by id) as t
+  GROUP BY d_rank;
+
+
 SELECT id, num,
 Dense_RANK() OVER (ORDER BY num) AS d_rank
-  FROM check1 c
-  order by id;
-
+  FROM check1
+  order by id
