174f11bebcce65da4e370aed42696bca1569af3a
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-11 22:11:23 +0000

    go

--- Exercise Files/check1.sql
+++ Exercise Files/check1.sql
@@ -15,3 +15,22 @@ SELECT id, num,
 Dense_RANK() OVER (ORDER BY num) AS d_rank
   FROM check1
   order by id
+
+
+SELECT id
+FROM (
+    SELECT 
+        num,
+        id,
+        LAG(num, 1) OVER (ORDER BY id) AS p_1,
+        LAG(num, 2) OVER (ORDER BY id) AS p_2
+    FROM check1
+) sub
+WHERE num = p_1 AND num = p_2;
+
+SELECT 
+        num,
+        id,
+        LAG(num, 1) OVER (ORDER BY id) AS p_1,
+        LAG(num, 2) OVER (ORDER BY id) AS p_2
+    FROM check1
