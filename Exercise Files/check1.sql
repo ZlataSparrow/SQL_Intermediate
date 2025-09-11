23b13441acaa8f4196fded805fd80d6d6412894a
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-11 23:54:32 +0000

    chekc

--- Exercise Files/check1.sql
+++ Exercise Files/check1.sql
@@ -4,4 +4,41 @@ select * from Users;
 
 canc_rate = Number_cancelled/Number_requests
 
-select 
+select request_at, count(*)
+from trips 
+GROUP BY request_at
+
+select t.request_at, count(*) as Number_cancelled 
+from Trips t
+JOIN Users u
+ON t.client_id = u.users_id or t.driver_id=u.users_id
+WHERE u.banned='No' AND (t.status IN ('cancelled_by_driver', 'cancelled_by_client'))
+GROUP BY t.request_at
+
+
+
+select w.request_at, count(*) as Number_requests 
+FROM(
+    SELECT t.request_at, t.id
+    FROM Trips t
+    JOIN Users u
+    ON t.client_id = u.users_id OR t.driver_id=u.users_id
+    WHERE u.banned = 'No'
+    GROUP BY t.request_at, t.id
+    ) w
+GROUP BY w.request_at
+
+
+SELECT t.request_at, t.id
+    FROM Trips t
+    JOIN Users u
+    ON t.client_id = u.users_id OR t.driver_id=u.users_id
+    WHERE u.banned = 'No'
+    GROUP BY t.request_at, t.id
+
+select t.request_at, count(*) as Number_cancelled 
+from Trips t
+JOIN Users u
+ON t.client_id = u.users_id or t.driver_id=u.users_id
+WHERE u.banned='No' AND (t.status IN ('cancelled_by_driver', 'cancelled_by_client'))
+GROUP BY t.request_at
