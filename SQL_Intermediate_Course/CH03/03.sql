e4780ef4ba3decc86dba4ece1b8963101bbc2090
Zlatagaga <112367472+ZlataSparrow@users.noreply.github.com>
2025-09-28 21:36:52 +0000

    sql practice

---
+++ SQL_Intermediate_Course/CH03/03.sql
@@ -0,0 +1,14 @@
+SELECT salary
+FROM data_sci.employees;
+
+
+SELECT avg(salary)
+FROM data_sci.employees
+
+SELECT trunc(avg(salary), 2)
+FROM data_sci.employees
+
+SELECT ceil(avg(salary))
+FROM data_sci.employees
+
+
