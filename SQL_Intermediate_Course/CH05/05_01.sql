6992c46e4d607d9be6f6bfb74aa862e70f0c9e48
Zlatagaga <zlatasparrow@gmail.com>
2025-12-01 11:52:15 -0800

    Refactor SQL query to calculate minimum salary per department and improve structure for clarity

--- SQL_Intermediate_Course/CH05/05_01.sql
+++ SQL_Intermediate_Course/CH05/05_01.sql
@@ -2,6 +2,20 @@
    department_id,
    last_name,
    salary,
-   first_value(salary) over (partition by department_id order by salary asc) first_sal
+   first_value(salary) 
+   OVER (PARTITION BY department_id ORDER BY salary) as min
 from
    data_sci.employees;
+
+
+select distinct department_id, min
+from
+(select
+   department_id,
+   last_name,
+   salary,
+   first_value(salary) 
+   OVER (PARTITION BY department_id ORDER BY salary) as min
+from
+   data_sci.employees) sub
+order by department_id
