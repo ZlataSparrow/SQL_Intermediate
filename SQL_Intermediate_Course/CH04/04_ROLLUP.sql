74f308aea5951bb177c2d98b9e89ff0e08f0d992
Zlatagaga <zlatasparrow@gmail.com>
2025-11-30 20:49:32 -0800

    ROLLUP

--- SQL_Intermediate_Course/CH04/04_ROLLUP.sql
+++ SQL_Intermediate_Course/CH04/04_ROLLUP.sql
@@ -1,4 +1,15 @@
-SELECT department_id, salary
-FROM data_sci.employees e1
-WHERE 
-(SELECT MAX(salary) FROM data_sci.employees e2) = e1.salary
+SELECT 
+    cr.country_name,cr.region_name,count(e.*)
+FROM data_sci.employees e 
+JOIN data_sci.company_regions cr
+ON e.region_id = cr.id
+GROUP BY cr.country_name, cr.region_name;
+
+SELECT 
+    cr.country_name,cr.region_name,count(e.*)
+FROM data_sci.employees e 
+JOIN data_sci.company_regions cr
+ON e.region_id = cr.id
+GROUP BY ROLLUP(cr.country_name, cr.region_name)
+ORDER BY cr.country_name, cr.region_name;
+
