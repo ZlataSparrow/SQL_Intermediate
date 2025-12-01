a429492d11a8adfa59feb7b4e0e7a58686dffbaf
Zlatagaga <zlatasparrow@gmail.com>
2025-12-01 13:46:13 -0800

    CTEs

---
+++ SQL_Intermediate_Course/CH06/06_02_CTE.sql
@@ -0,0 +1,17 @@
+﻿WITH region_salaries AS
+  (SELECT 
+    region_id, 
+    SUM(salary) as region_salary
+  FROM data_sci.employees
+  GROUP BY region_id),
+
+top_region_salaries AS
+  (SELECT
+    region_id
+   FROM region_salaries
+   WHERE region_salary > (SELECT AVG(region_salary) FROM region_salaries))
+
+
+SELECT * FROM top_region_salaries
+WHERE region_id IN (SELECT region_id FROM region_salaries);
+
